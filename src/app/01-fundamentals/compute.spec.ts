ad598b6f6a0fef134c1957392cc81a5e772bf727
Wolodja <wolodjawaskovych@i.ua>
2020-10-12 15:09:53 +0200

    First tests

--- src/app/01-fundamentals/compute.spec.ts
+++ src/app/01-fundamentals/compute.spec.ts
@@ -1,9 +1,9 @@
-import { compute } from "./compute"
+import { compute } from './compute';
 
 describe('compute', () => {
     it('shpuld return 0 when input is negative', () => {
         const result = compute(-1);
-        expect(result).toBe(0); 
-    })
+        expect(result).toBe(0);
+    });
 
-})
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('integration-test-demo');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('integration-test-demo app is running!');
-  });
+
 });

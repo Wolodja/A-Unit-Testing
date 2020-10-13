daff6f325588c999ac55cb35c1d9a4144ffac829
Wolodja <wolodjawaskovych@i.ua>
2020-10-13 18:27:00 +0200

    Working with form

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,23 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  var component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should create form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTrue();
+    expect(component.form.contains('email')).toBeTrue();
   });
 
-  it('', () => {
+  it('should make name control required', () => {
+    let control = component.form.get('name');
+
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
   });
 });
